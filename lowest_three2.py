6b3232b2a7f4af35472ce86ffc0a608407da0971
baraahekal <ibaraahekal@gmail.com>
2023-09-25 23:07:29 +0300

    too simple version but complexuty O(n log(n))

--- lowest_three2.py
+++ lowest_three2.py
@@ -4,9 +4,8 @@ def find_lowest_three(lst):
 
 if __name__ == '__main__':
     lst = list(map(int, input().split()))
-
-    min_lst = find_lowest_three(lst)
-    print(min_lst)
+    print(find_lowest_three(lst)
+)
 
 
 
