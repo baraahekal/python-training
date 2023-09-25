a5eeadd2f26f6509b074402cf72248097a6385cc
baraahekal <ibaraahekal@gmail.com>
2023-09-25 06:52:15 +0300

    getting the min 3 values in an list in a linear time complexity O(len(list))

--- lowest_three.py
+++ lowest_three.py
@@ -1,4 +1,7 @@
 def find_lowest_three(lst):
+    if len(lst) <= 3:
+        return lst
+
     min_lst = [None] * 3
     min_lst[0], min_lst[1], min_lst[2] = lst[0], lst[1], lst[2]
     min_lst.sort()
@@ -10,6 +13,7 @@ def find_lowest_three(lst):
             min_lst[1] = lst[i]
         elif lst[i] < min_lst[2]:
             min_lst[2] = lst[i]
+
     return min_lst
 
 if __name__ == '__main__':
