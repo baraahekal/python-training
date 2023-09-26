31eb50605646b6d66b198453a5ad83175ec9f5ad
baraahekal <ibaraahekal@gmail.com>
2023-09-26 10:01:01 +0300

    learning a very interesting new syntax 'list comprehension'

---
+++ interesting_list_comprehension.py
@@ -0,0 +1,7 @@
+lst = [-1, 2, 3, -2, 0, 5, -3, 8]
+
+new_lst = [element for element in lst if element > 0]
+print(new_lst)
+
+# this new syntax takes every element in [lst] that greater than 0
+# and append it to the [new_lst]
